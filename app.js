8e6f3abd735d7716bc1e1d8dd3b314a0c68058c4
Eitan Chmelevsky <eitan4562@gmail.com>
2021-03-03 22:40:51 +0200

    Navbar done

---
+++ app.js
@@ -0,0 +1,25 @@
+/* Navbar functionality */
+
+const toggle = document.querySelector(".toggle");
+const menu = document.querySelector(".menu");
+
+/* Toggle mobile menu */
+
+function toggleMenu() {
+    // Check if the menu is active right now
+    if (menu.classList.contains("active")) {
+        menu.classList.remove("active");
+        
+        // Adds the menu hamburger icon back
+        toggle.querySelector("a").innerHTML = "<i class='fas fa-bars'></i>";
+    }
+    else {
+        menu.classList.add("active");
+        
+        // Replaces hamburger icon with x icon
+        toggle.querySelector("a").innerHTML = "<i class='fas fa-times'></i>";
+    }
+}
+
+/* Event listener */
+toggle.addEventListener("click", toggleMenu, false);
