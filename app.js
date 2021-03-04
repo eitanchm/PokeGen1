9e671c03dd9b98b8e8b67746a5e169d30f425454
Eitan Chmelevsky <eitan4562@gmail.com>
2021-03-04 13:52:09 +0200

    Scrap the current project, transform to a single page mini web app

--- app.js
+++ app.js
@@ -1,36 +1,7 @@
-/* Navbar functionality */
-
-const toggle = document.querySelector(".toggle");
-const menu = document.querySelector(".menu");
-
-/* Toggle mobile menu */
-
-function toggleMenu() {
-    // Check if the menu is active right now
-    if (menu.classList.contains("active")) {
-        menu.classList.remove("active");
-        
-        // Adds the menu hamburger icon back
-        toggle.querySelector("a").innerHTML = "<i class='fas fa-bars'></i>";
-    }
-    else {
-        menu.classList.add("active");
-        
-        // Replaces hamburger icon with x icon
-        toggle.querySelector("a").innerHTML = "<i class='fas fa-times'></i>";
-    }
-}
-
-/* Event listener */
-toggle.addEventListener("click", toggleMenu, false);
-
-
 /* Pokemon grid functionality */
 
 const pokemonCard = document.querySelector(".pokemon-card");
 
 /* Fill card with information and HTML */
 
-function getPokemon() {
-    
-}
+const fetchPokemon = () =>
