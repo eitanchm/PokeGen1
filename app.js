3e20a7ff6e2c3bcbdcc9c973e812435380d1e90d
Eitan Chmelevsky <eitan4562@gmail.com>
2021-03-05 12:35:45 +0200

    Grid done, pokemons fetched from API

--- app.js
+++ app.js
@@ -1,7 +1,40 @@
 /* Pokemon grid functionality */
 
-const pokemonCard = document.querySelector(".pokemon-card");
+const pokemonCard = document.querySelector(".grid-container");
 
-/* Fill card with information and HTML */
+/* Function to fetch pokemons from PokeAPI and fill pokemon card with info and HTML */
+function fetchPokemon() {
+    for (let id = 1; id <= 151; id++)
+    {
+        let pokeUrl = `https://pokeapi.co/api/v2/pokemon/${id}`;
 
-const fetchPokemon = () =>
+        fetch(pokeUrl)
+            .then(res => {
+                return res.json();
+            })
+            // Build a pokemon object
+            .then(data => {
+                const pokemon = {};
+                pokemon['name'] = data.name;
+                pokemon['image'] = data.sprites.front_default;
+                pokemon['types'] = data.types;
+                
+                // Make a readable string of all the pokemon's types
+                let types = '';
+                for (i in pokemon['types']) {
+                    types += `${pokemon['types'][i].type.name}, `;
+                }
+                // Remove the last ', '
+                types = types.slice(0, -2);
+
+                // Add the innerHTML for each pokemon card
+                pokemonCard.innerHTML += `<div class="pokemon-card"><img src="${pokemon['image']}" />
+                    <h3 class="pokemon-name">${id}. ${pokemon['name'].charAt(0).toUpperCase() + pokemon['name'].slice(1)}
+                    </h3><p class="pokemon-types">Type: ${types}</p></div>`;
+
+            });
+    }
+};
+
+/* Function call */
+fetchPokemon();
